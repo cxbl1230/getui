9c595cee6a08779ecf88c6f260ce8a8ffb867ac7
xiao.Chen <cxbl1230@qq.com>
2018-06-22 13:08:58 +0800

    add

---
+++ autoload.php
@@ -0,0 +1,7 @@
+<?php
+require_once(dirname(__FILE__) .'/src/' . 'IGt.Push.php');
+require_once(dirname(__FILE__) .'/src/' . 'igetui/IGt.AppMessage.php');
+require_once(dirname(__FILE__) .'/src/' . 'igetui/IGt.APNPayload.php');
+require_once(dirname(__FILE__) .'/src/' . 'igetui/template/IGt.BaseTemplate.php');
+require_once(dirname(__FILE__) .'/src/' . 'IGt.Batch.php');
+require_once(dirname(__FILE__) .'/src/' . 'igetui/utils/AppConditions.php');
